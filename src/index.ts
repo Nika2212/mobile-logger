9bb05789d06354c9bac7ed8ebd523e8eadb40a50
nbarsukovi <nbarsukovi@crocobet.com>
2021-04-12 11:20:58 +0400

    This is a Commit

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,12 @@ class StaticDataContainer {
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bug" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-bug fa-w-16"><path fill="#f8f8f8" d="M511.988 288.9c-.478 17.43-15.217 31.1-32.653 31.1H424v16c0 21.864-4.882 42.584-13.6 61.145l60.228 60.228c12.496 12.497 12.496 32.758 0 45.255-12.498 12.497-32.759 12.496-45.256 0l-54.736-54.736C345.886 467.965 314.351 480 280 480V236c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v244c-34.351 0-65.886-12.035-90.636-32.108l-54.736 54.736c-12.498 12.497-32.759 12.496-45.256 0-12.496-12.497-12.496-32.758 0-45.255l60.228-60.228C92.882 378.584 88 357.864 88 336v-16H32.666C15.23 320 .491 306.33.013 288.9-.484 270.816 14.028 256 32 256h56v-58.745l-46.628-46.628c-12.496-12.497-12.496-32.758 0-45.255 12.498-12.497 32.758-12.497 45.256 0L141.255 160h229.489l54.627-54.627c12.498-12.497 32.758-12.497 45.256 0 12.496 12.497 12.496 32.758 0 45.255L424 197.255V256h56c17.972 0 32.484 14.816 31.988 32.9zM257 0c-61.856 0-112 50.144-112 112h224C369 50.144 318.856 0 257 0z" class=""></path></svg>
         `;
     }
+
+    static getCloseSVG(): string {
+        return `
+            <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-times fa-w-10"><path fill="#f8f8f8" d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z" class=""></path></svg>
+        `;
+    }
 }
 
 class DOM {
@@ -237,7 +243,7 @@ class MobileLogger {
             fontColor = '#ff8080';
         }
 
-        this.inspectModalRef.innerHTML += `<p style="box-sizing: border-box; user-select: text; display: inline-block; width: 100%; max-width: 100%; white-space: pre-wrap; min-height: 20px; padding-top: 1px; padding-bottom: 1px; padding-left: 4px; margin: 0; font-family: consolas, sans-serif; font-size: 12px; line-height: 15px; color: ${fontColor}; ${boxStyle};">${formattedLog}</p>`;
+        this.inspectModalRef.innerHTML += `<p style="box-sizing: border-box; user-select: text; display: inline-block; width: 100%; max-width: 100%; white-space: pre-wrap; min-height: 20px; padding-top: 1px; padding-bottom: 1px; padding-left: 4px; margin: 0; font-family: consolas, sans-serif; font-size: 12px; line-height: 15px; flex-shrink: 0; color: ${fontColor}; ${boxStyle};">${formattedLog}</p>`;
         this.inspectModalRef.scroll(0, this.inspectModalRef.scrollHeight);
     }
 
@@ -247,16 +253,9 @@ class MobileLogger {
 
         if (this.modalState) {
             this.inspectModalRef.scroll(0, this.inspectModalRef.scrollHeight);
+        } else {
         }
     }
 }
 
 const _md = new MobileLogger();
-
-for (let i = 0; i < 100; i++) {
-    console.log('Info');
-    console.warn('Warning');
-    console.error('Error');
-}
-
-console.error('Last error');
